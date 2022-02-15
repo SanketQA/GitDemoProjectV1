102bcea3c6999abe4c43b30eeaa07d90f25e953f
Sanket <sanketpawar7979@gmail.com>
2022-02-16 00:35:43 +0530

    Commiting Test2

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	{
+		int []a={23,20,45,36,85,63};
+		Arrays.sort(a);
+		for(int b:a)
+		{
+			System.out.println(b);
+		}
+		
+
+	}
+
+}
