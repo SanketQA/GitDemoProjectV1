dc92633cdaa9801e5efbbb60804c8aee3a10a7bb
Sanket <sanketpawar7979@gmail.com>
2022-02-23 20:35:02 +0530

    Commited Test4

---
+++ src/Test4.java
@@ -0,0 +1,14 @@
+
+public class Test4 {
+
+	public static void main(String[] args) 
+	{
+		String s="welcome-to-Pune";
+		String []t=s.split("-");
+		for(String p:t)
+		{
+			System.out.println(p);
+		}
+	}
+  
+    }
